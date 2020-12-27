56e06c77e554487b77d038f0feb8d7c5181b5122
Njunu-sk <simonnjunu16@gmail.com>
2020-12-27 20:40:31 +0300

    add cart functionality

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,7 @@
 class Instrument < ApplicationRecord
-
+    before_destroy :not_referenced_by_any_line_item
     belongs_to :user, optional: true
+    has_many :line_items
 
     mount_uploader :image, ImageUploader
     serialize :image, JSON
@@ -14,4 +15,11 @@ class Instrument < ApplicationRecord
     FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
     CONDITION = %w{ New Excellent Mint Used Fair Poor }
 
+    private
+    def not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, "Line items present")
+            throw :abort
+        end
+    end
 end
